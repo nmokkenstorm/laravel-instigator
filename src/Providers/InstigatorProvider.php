a3df89dd1be4eb69de17ec0fb40d1638e3eded0a
Niels Mokkentstorm <nielsmokkenstorm@gmail.com>
2020-03-10 21:48:03 +0100

    Initial project setup

---
+++ src/Providers/InstigatorProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Instigator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class InstigatorProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
+
